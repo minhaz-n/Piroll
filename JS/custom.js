8082ce4f96309fe1b43bf4f44b676a3adf264c59
minhaz-n <minhazvictorians@gmail.com>
2024-10-31 22:15:39 +0600

    first commit

---
+++ JS/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.cus-asked').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: false
+      });
+});
